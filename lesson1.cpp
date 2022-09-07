e1b4d6eb76af2520ac1bad9c3f59dd4f6a667dd1
D3c3453d <sergey.et02@gmail.com>
2022-09-07 22:29:07 +0300

    feat: lesson1

--- lesson1.cpp
+++ lesson1.cpp
@@ -1,32 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
+#include "iostream"
+#include "vector"
+#include "queue"
 
-class IGraph{
+
+class IGraph
+{
 public:
-    virtual size_t size()=0;
-    virtual void AddEdge(int to, int from)=0;
-    virtual std::vector<int> GetEdge(int v)=0;
+    virtual size_t size() = 0;
+    virtual void AddEdge(int to, int from) = 0;
+    virtual std::vector<int> GetEdge(int v) = 0;
 };
 
-class OrMGraph : IGraph{
+
+class OrMGraph : public IGraph
+{
 private:
-    std::vector<std::vector<bool>> matrix{};
-
+    std::vector<std::vector<bool>> matrix {};
 public:
-    OrMGraph(int n):matrix(n, std::vector<bool>(n, false)){}
-
-    size_t size() override {return matrix.size();}
-
-    void AddEdge (int to, int from) override{
+    OrMGraph(int n): matrix(n, std::vector<bool> (n, false)) {};
+    size_t size() override  { return matrix.size();};
+    void AddEdge(int to, int from)
+    {
         matrix[from][to] = true;
     }
-
-    std::vector<int> GetEdge(int v) override{
+    std::vector<int> GetEdge(int v) override
+    {
         std::vector<int> edges{};
-        for(int i=0; i < matrix.size(); ++i){
-            if(matrix[v][i])
+        for (int i = 0; i < matrix.size(); ++i)
+        {
+            if (matrix[v][i])
                 edges.push_back(i);
         }
         return edges;
@@ -34,41 +36,35 @@ public:
 };
 
 
-void dfs(IGraph& g, int v, int p){
+void dfs(IGraph& g, int v, int p)
+{
     std::vector<std::string> color(g.size());
     color[v] = "gray";
-    for(int i: g.GetEdge(v)){
+    for (int i: g.GetEdge(v))
+    {
         if (color[i] == "white")
             dfs(g, i, v);
     }
     color[v] = "black";
 }
 
-void bfs(IGraph& g, int v){
-    std::vector<std::string> colors(g.size(), "white");
-    std::queue<int> q;
-    q.push(v);
-    while (!q.empty()) {
-        v = q.front();
-        for(auto i: g.GetEdge(v))
-            if(colors[i]=="white")
-                q.push(v);
-    }
-}
 
-std::vector<int> BFS(IGraph& G, int v){
-    std::vector<int> dist (G.size() - 1);
-    std::vector<std::string> colors(G.size(), "white");
+auto bfs(IGraph& g, int v)
+{
+    std::vector<int> dist (g.size() - 1);
+    std::vector<std::string> color(g.size(), "white");
     std::queue<std::pair<int, int>> q;
     q.push({v, 0});
-    while(!q.empty()){
-        std::pair<int,int> tmp = q.front();
+    while ( !q.empty() )
+    {
+        auto tmp = q.front();
         v = tmp.first;
         int d = tmp.second;
-        colors[v] = "black";
-        for (int i: G.GetEdge(v)){
-            if (colors[i]=="white")
-                q.push({i, d+1});
+        color[v] = "black";
+        for (auto i: g.GetEdge(v))
+        {
+            if (color[i] == "white")
+                q.push({i, d + 1});
         }
         dist[v] = d;
         q.pop();
@@ -76,12 +72,23 @@ std::vector<int> BFS(IGraph& G, int v){
     return dist;
 }
 
-int main() {
+
+
+
+int main()
+{
     int v_num, e_num;
-    int s, l, m;
+    int S, L, M;
     std::cin >> v_num >> e_num;
     OrMGraph G(v_num);
-    //auto
-
-    //for(int i = )
-}
+    auto s_length = bfs(G, S);
+    auto l_length = bfs(G, L);
+    auto m_length = bfs(G, M);
+    int min_dist = s_length[0] + m_length[0] + l_length[0];
+    for (int i = 0; i < v_num; ++i)
+    {
+        min_dist = std::min(min_dist, s_length[i] + m_length[i] + l_length[i]);
+    }
+    std::cout << min_dist;
+    return 0;
+};
